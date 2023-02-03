f0f9193d1a185709a7361adb11aa872cd2d6d519
Guudz <Kilian1702@outlook.com>
2023-02-03 04:07:29 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'adamant'
+game {'gta5'}
+
+description 'Scrubz Hud'
+name 'scrubz_hud'
+author 'Scrubz#0001'
+version 'v1.0'
+
+client_script 'client/*.lua'
+
+ui_page 'ui/index.html'
+
+files {
+    'ui/index.html',
+    'ui/static/css/main.css',
+    'ui/static/js/main.js',
+    'ui/static/media/*.svg'
+}
+
+
+
+-- Nova Leaks discord.gg/2vubhJDFfh
